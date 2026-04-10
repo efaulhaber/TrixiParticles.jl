99ab1cd3b1a2713a50bef4748562757ac1654422
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2026-04-10 10:06:13 +0200

    Fix

--- src/schemes/fluid/viscosity.jl
+++ src/schemes/fluid/viscosity.jl
@@ -122,7 +122,8 @@ end
         # See the docs page "Development" for more details on `div_fast`.
         mu = div_fast(h * vr, distance^2 + epsilon * h^2)
         c = sound_speed
-        dv_viscosity = div_fast(m_b * alpha * c * mu + beta * mu^2, rho_mean) * grad_kernel
+        # TODO why is m_b inside the `div_fast` faster on H100 than `m_b * div_fast(...)`?
+        dv_viscosity = div_fast(m_b * alpha * c * mu + m_b * beta * mu^2, rho_mean) * grad_kernel
         dv_particle[] += viscosity_correction * dv_viscosity
     end
 
