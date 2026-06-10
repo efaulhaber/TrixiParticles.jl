f8338174f25366b095ab985e8482b1832d3eeb0a
Erik Faulhaber <44124897+efaulhaber@users.noreply.github.com>
2026-06-10 12:52:28 +0200

    Fix fin NHS

--- examples/fsi/fin_2d.jl
+++ examples/fsi/fin_2d.jl
@@ -356,7 +356,7 @@ neighborhood_search = GridNeighborhoodSearch{2}(; periodic_box, cell_list,
                                                 update_strategy=ParallelUpdate())
 
 semi = Semidiscretization(fluid_system, boundary_system, open_boundary_system, structure_system;
-                          neighborhood_search=PrecomputedNeighborhoodSearch{2}(update_neighborhood_search=neighborhood_search),
+                          neighborhood_search=neighborhood_search,
                           parallelization_backend=PolyesterBackend())
 ode = semidiscretize(semi, tspan)
 
